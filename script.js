d019dfd9cde3756f04227f820a2cac275a2c1f00
Sajeesh <sajeeshsaju003@gmail.com>
2023-10-05 01:04:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function buttonclick(val)
+{
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+function clearDisplay()
+{
+    document.getElementById("screen").value=('');
+}
+function equalClick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
+function plusClick()
+{
+    if(plusClick=true)
+    var text=document.getElementById("screen").value
+    var text=document.getElementById("sreen").value
+}
